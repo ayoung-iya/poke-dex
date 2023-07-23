be557e324661fd3a9eb963049f97b37e9175116c
YunAYoung <a1878h@gmail.com>
2023-07-24 01:39:01 +0900

    API에서 받은 정보로 메인 페이지의 카드칩들에 적용하기

--- src/List/PokeCard.tsx
+++ src/List/PokeCard.tsx
@@ -1,35 +1,53 @@
-import styled from "@emotion/styled"
-import PokeNameChip from "../Common/PokeNameChip"
+import styled from "@emotion/styled";
+import PokeNameChip from "../Common/PokeNameChip";
 import PokeMarkChip from "../Common/PokeMarkChip";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  PokemonDetailType,
+  fetchPokemonDetail,
+} from "../Service/pokemonService";
 
-const TempImgUrl = 'https://mblogthumb-phinf.pstatic.net/20160817_259/retspe_14714118890125sC2j_PNG/%C7%C7%C4%AB%C3%F2_%281%29.png?type=w800';
+const TempImgUrl =
+  "https://mblogthumb-phinf.pstatic.net/20160817_259/retspe_14714118890125sC2j_PNG/%C7%C7%C4%AB%C3%F2_%281%29.png?type=w800";
 
 interface PokeCardProps {
-  name: string
+  name: string;
 }
 
-const PokeCard = (props:PokeCardProps) => {
+const PokeCard = (props: PokeCardProps) => {
   const navigate = useNavigate();
+  const [pokemon, setPokemon] = useState<PokemonDetailType | null>(null);
 
   const handleClick = () => {
     navigate(`/pokemon/${props.name}`);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const detail = await fetchPokemonDetail(props.name);
+      setPokemon(detail);
+    })();
+  }, [props.name]);
+
+  if (!pokemon) {
+    return null; // TODO: 화면이 로딩중일 때 표시
   }
-  
+
   return (
     <Item onClick={handleClick}>
       <Header>
-        <PokeNameChip name={props.name}/>
+        <PokeNameChip name={pokemon.name} id={pokemon.id} />
       </Header>
       <Body>
-        <Image src={TempImgUrl} alt="이상해씨 이미지"/>
+        <Image src={pokemon.images.dreamWorldDefault} alt={pokemon.name} />
       </Body>
       <Footer>
         <PokeMarkChip />
       </Footer>
     </Item>
-  )
-}
+  );
+};
 
 const Item = styled.li`
   display: flex;
@@ -39,7 +57,7 @@ const Item = styled.li`
 
   width: 250px;
   height: 300px;
-  
+
   border: 1px solid #c0c0c0;
   box-shadow: 1px 1px 3px 1px #c0c0c0;
 
@@ -55,30 +73,29 @@ const Item = styled.li`
     opacity: 0.8;
     tansition: background-color 0s;
   }
-`
+`;
 
-const Header = styled.section `
+const Header = styled.section`
   display: flex;
   flex-direction: row;
   margin: 8px 0;
-`
+`;
 const Body = styled.section`
   display: flex;
   flex: 1 1 auto;
   justify-content: center;
   align-items: center;
   margin: 8px 0;
-`
+`;
 const Image = styled.img`
   width: 180px;
   height: 180px;
-
-`
+`;
 
 const Footer = styled.section`
   display: flex;
   flex-direction: row;
   margin: 8px 0;
-`
+`;
 
-export default PokeCard
+export default PokeCard;
--- src/Service/pokemonService.ts
+++ src/Service/pokemonService.ts
@@ -18,3 +18,75 @@ export const fetchPokemons = async () => {
 
   return response.data;
 };
+
+interface PokemonDetailResponseType {
+  id: number,
+  weight: number,
+  height: number,
+  name: string,
+  types: {
+    type: {
+      name: string,
+    }
+  }[],
+  sprites: {
+    front_default: string,
+    other: {
+      dream_world: {
+        front_default: string
+      }
+      'official-artwork': {
+        front_default: string
+      }
+    }
+  },
+  stats: {
+    base_stat: number,
+    stat: {
+      name: string
+    }
+  }[],
+}
+
+export interface PokemonDetailType {
+  id: number,
+  weight: number,
+  height: number,
+  name: string,
+  types: string[],
+  images: {
+    frontDefault: string,
+    dreamWorldDefault: string,
+    OfficialArtworkFront: string
+  },
+  baseState: {
+    name: string,
+    value: number
+  }[]
+}
+
+export const fetchPokemonDetail = async (name:string):Promise<PokemonDetailType> => {
+  const pokemonDetailUrl = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  const response = await remote.get<PokemonDetailResponseType>(pokemonDetailUrl);
+  const detail = response.data;
+
+  return {
+    id: detail.id,
+    name: detail.name,
+    height: detail.height / 10,  //미터단위
+    weight: detail.weight / 10, // kg 단위
+    types: detail.types.map(item => item.type.name),
+    images: {
+      frontDefault: detail.sprites.front_default,
+      dreamWorldDefault: detail.sprites.other.dream_world.front_default,
+      OfficialArtworkFront: detail.sprites.other["official-artwork"].front_default
+    },
+    baseState: detail.stats.map(item => {
+      return {
+        name: item.stat.name,
+        value: item.base_stat
+      }
+    })
+  }
+  console.log(response);
+}
